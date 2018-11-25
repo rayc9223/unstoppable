c0aa173f3f398fce2a661fb684137fc5f98c7c94
ray <rayc9223@gmail.com>
2018-11-25 17:18:13 +0800

    index link horizontal padding set to 20px

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
 
             .links > a {
                 color: #636b6f;
-                padding: 0 25px;
+                padding: 0 20px;
                 font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
