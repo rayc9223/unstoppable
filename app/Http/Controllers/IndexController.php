28843c062b05e49d9359ef92c0926591028e4532
ray <rayc9223@gmail.com>
2018-11-28 00:22:15 +0800

    test

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -182,7 +182,7 @@ class IndexController extends Controller
         $announcement = new Announcement();
         $announcement->type = $request->type;
         $announcement->uid  = Auth::user()->uid;
-        $announcement->content = str_replace(["\r\n", "\n", "\r"], '<br>', $request->content);
+        $announcement->content = $request->content;
         $announcement->updated_by = $author;
         $announcement->last_update = time();
         $announcement->save();
