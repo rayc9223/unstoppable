57ac5a9aa2343bdfdbf224c9d81765957c971056
ray <rayc9223@gmail.com>
2018-12-17 20:52:35 +0800

    Line Event

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -9,13 +9,12 @@ class LineController extends Controller
 {
     public function index(Request $request)
     {
-        return Response::json('hello', 200);
         // http_response_code(200);
         // file_put_contents('logs.txt', $request->all());
     }
 
     public function lineEvent(Request $request)
     {
-        return Response::json('hello', 200);
+        
     }
 }
