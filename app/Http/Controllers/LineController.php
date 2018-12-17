851efe3912d3e8e014f7885048417eb87b1f06ef
ray <rayc9223@gmail.com>
2018-12-17 21:39:05 +0800

    Line Event

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -18,5 +18,32 @@ class LineController extends Controller
     public function lineEvent(Request $request)
     {
         Log::info(json_encode($request->all()));
+        $event = json_decode($request->all(), true);
+        Log::info(json_encode($event['events']['replyToken']));
+
     }
+
+//     [2018-12-17 13:22:34] local.INFO: {"events":[{"replyToken":"00000000000000000000000000000000","type":"message","timestamp":1545053246198,"source":{"type":"user","userId":"Udeadbeefdeadbeefdeadbeefdeadbeef"},"message":{"id":"100001","type":"text","text":"Hello, world"}},{"replyToken":"ffffffffffffffffffffffffffffffff","type":"message","timestamp":1545053246198,"source":{"type":"user","userId":"Udeadbeefdeadbeefdeadbeefdeadbeef"},"message":{"id":"100002","type":"sticker","packageId":"1","stickerId":"1"}}]}  
+
+// [2018-12-17 13:22:56] local.INFO: {"events":[{"type":"follow","replyToken":"d68a1293fffb484593837bcdf162173f","source":{"userId":"U1b7997d75ba52775e41438aa1d502150","type":"user"},"timestamp":1545053267719}],"destination":"U27c9098d14de1f99fd2f750548cc388d"}  
+
+// [2018-12-17 13:22:58] local.INFO: {"events":[{"type":"unfollow","source":{"userId":"U1b7997d75ba52775e41438aa1d502150","type":"user"},"timestamp":1545053269958}],"destination":"U27c9098d14de1f99fd2f750548cc388d"}  
+
+// [2018-12-17 13:26:54] local.INFO: {"events":[
+//     {
+//         "type":"message",
+//         "replyToken":"f89bc63504bb45cda0aac51ab9a667c2",
+//         "source":{
+//             "userId":"U1b7997d75ba52775e41438aa1d502150",
+//             "type":"user"
+//         },
+//         "timestamp":1545053506139,
+//         "message":{
+//             "type":"text",
+//             "id":"9023507402074",
+//             "text":"test message no.1"
+//         }}],
+//         "destination":"U27c9098d14de1f99fd2f750548cc388d"
+//     }
+
 }
