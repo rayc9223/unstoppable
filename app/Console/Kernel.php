e91e4c2ef508e912b1726ab4fa2458de492823ec
ray <rayc9223@gmail.com>
2019-02-18 22:00:51 +0800

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        'auto_leave'
     ];
 
     /**
@@ -25,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('auto_leave')
-                 ->mondays()->at('21:52');
+                 ->mondays()->at('22:03');
     }
 
     /**
