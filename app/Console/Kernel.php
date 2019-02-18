07807897c2a156acb75695db34111e39862d17d4
ray <rayc9223@gmail.com>
2019-02-18 21:27:55 +0800

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends ConsoleKernel
                 $leave->save();
             }        
         // })->weekly()->tuesdays()->thursdays()->saturdays()->at('21:00');
-        })->weekly()->mondays()->at('21:24');
+        })->mondays()->at('21:30');
     }
 
     /**
